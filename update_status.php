8fa97508ead3a5e75efdd7bd013bc97751409953
zec0816 <tzecv0422@gmail.com>
2024-12-22 21:07:56 +0800

    Add which request accepted by which volunteer in db

--- update_status.php
+++ update_status.php
@@ -1,14 +1,38 @@
 <?php
-require 'connection.php';
+$hostName = "localhost";
+$userName = "root";
+$password = "";
+$dbName = "helpafriend";
 
-$id_location = $_POST['id_location'];
-$status = $_POST['status'];
+$connection = mysqli_connect($hostName, $userName, $password, $dbName);
 
-// Update the status of the location
-$sql = "UPDATE locations SET status = '$status' WHERE id_location = $id_location";
-if (mysqli_query($connection, $sql)) {
-    echo json_encode(["success" => true, "message" => "Status updated successfully"]);
-} else {
-    echo json_encode(["success" => false, "message" => mysqli_error($connection)]);
+if (!$connection) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $id_location = mysqli_real_escape_string($connection, $_POST['id_location']);
+    $status = mysqli_real_escape_string($connection, $_POST['status']);
+    $username = mysqli_real_escape_string($connection, $_POST['username']);
+
+    // Retrieve the user ID based on the username
+    $userCheckQuery = "SELECT id_user FROM user WHERE username = '$username'";
+    $userResult = mysqli_query($connection, $userCheckQuery);
+    $user = mysqli_fetch_assoc($userResult);
+
+    if ($user) {
+        $volunteer_id = $user['id_user'];
+
+        // Update the status and set the volunteer_id
+        $updateQuery = "UPDATE locations SET status = '$status', accepted_by = '$volunteer_id' WHERE id_location = '$id_location'";
+        
+        if (mysqli_query($connection, $updateQuery)) {
+            echo "Request status updated successfully";
+        } else {
+            echo "Error: " . mysqli_error($connection);
+        }
+    } else {
+        echo "User not found";
+    }
 }
 ?>
